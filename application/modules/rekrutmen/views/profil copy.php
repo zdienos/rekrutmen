5bc86f1ec3e348033ba1cfca01acaae6553ea0de
zdienos <zdienos@yahoao.com>
2019-11-22 10:22:36 +0800

    'dikkit'

--- application/modules/rekrutmen/views/profil copy.php
+++ application/modules/rekrutmen/views/profil copy.php
@@ -821,8 +821,6 @@
 					
 					</div>
 					<!-- /Title -->
-
-					 
 					
 					<div class="row">
 					<div class="col-sm-12">
@@ -859,20 +857,12 @@
 											</div>											
 											<div class="form-group">
 												<label class="control-label mb-10 col-sm-2 col-xs-12">Jenis Kelamin</label>
-												<div class="col-sm-4 col-xs-6">																					
-													<div class="radio radio-info">
-														<input type="radio" name="radio" id="radio1" value="option1" checked="">
-														<label for="radio">Laki-laki														</label>
-														<input  type="radio" name="radio" id="radio2" value="option2" checked="">
-														<label style="margin-left:50px" for="radio2">Perempuan
-														</label>
-													</div>
-																											
-													<!-- <select class="selectpicker" data-style="form-control btn-default btn-outline">
+												<div class="col-sm-4 col-xs-6">
+													<select class="selectpicker" data-style="form-control btn-default btn-outline">
 													 		<option selected>Jenis Kelamin</option>
 															<option>Laki-laki</option>
 															<option>Perempuan</option>
-													</select> -->
+														</select>
 												</div>
 											</div>
 											<div class="form-group">
@@ -881,55 +871,6 @@
 													<input type="text" class="form-control"  placeholder="">
 												</div>
 											</div>
-											<div class="form-group mt-10 mb-10">
-												<label class="control-label mb-10 col-sm-2">Propinsi</label>
-												<div class="col-sm-4">
-													<select class="form-control" >
-														<option>Sulawesi Selatan</option>
-														<option>Sulawesi Barat</option>
-														<option>Sulawesi Utara</option>
-														<option>Sulawesi Tengah</option>
-														<option>Sulawesi Tenggara</option>
-													</select>
-												</div>
-											<!-- </div>
-											<div class="form-group mt-30 mb-30"> -->
-												<label class="control-label mt-10  col-sm-2">Kota</label>
-												<div class="col-sm-4">
-													<select class="form-control" >
-														<option>Makassar</option>
-														<option>Kendari</option>
-														<option>Palu</option>
-														<option>Gorontalo</option>
-														<option>Balikpapan</option>
-													</select>
-												</div>
-											</div>
-											<div class="form-group">
-												<label class="control-label mb-10 col-sm-2">No Handphone</label>
-												<div class="col-sm-10">
-													<input type="text" class="form-control"  placeholder="">
-												</div>
-											</div>
-											<div class="form-group">
-												<label class="control-label mb-10 col-sm-2">No KTP</label>
-												<div class="col-sm-10">
-													<input type="text" class="form-control"  placeholder="">
-												</div>
-											</div>
-											<div class="form-group mt-10 mb-10"> 
-												<label class="control-label mb-10  col-sm-2">Agama</label>
-												<div class="col-sm-4">
-													<select class="form-control" >
-														<option>Islam</option>
-														<option>Katolik</option>
-														<option>Protestan</option>
-														<option>Budha</option>
-														<option>Hindu</option>
-													</select>
-												</div>
-											</div>
-											
 												<!-- <div class="form-group">
 													<label class="control-label mb-10 col-sm-2" for="email_hr">Email:</label>
 													<div class="col-sm-10">
@@ -964,7 +905,106 @@
 							</div>
 						</div>
 					</div>
-					
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="panel panel-default card-view">
+								<div class="panel-heading">
+									<div class="pull-left">
+										<h6 class="panel-title txt-dark">Biodata</h6>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+								<div class="panel-wrapper collapse in">
+									<div class="panel-body">
+										<div class="form-wrap">
+											<form>
+												<div class="form-group">
+													<label class="control-label mb-10 text-left">Nama Lengkap</label>
+													<input type="text" class="form-control" value="">
+												</div>
+												<div class="form-group">
+													<label class="control-label mb-10 text-left" for="example-email">Email <span class="help"> e.g. "example@gmail.com"</span></label>
+													<input type="email" id="example-email" name="example-email" class="form-control" placeholder="Email">
+												</div>
+												<div class="form-group">
+													<label class="control-label mb-10 text-left">Password</label>
+													<input type="password" class="form-control" value="password">
+												</div>
+												<div class="form-group">
+													<label class="control-label mb-10 text-left">Placeholder</label>
+													<input type="text" class="form-control" placeholder="placeholder">
+												</div>
+												<div class="form-group">
+													<label class="control-label mb-10 text-left">Text area</label>
+													<textarea class="form-control" rows="5"></textarea>
+												</div>
+												<div class="form-group mt-30 mb-30">
+													<label class="control-label mb-10 text-left">select</label>
+													<select class="form-control">
+														<option>1</option>
+														<option>2</option>
+														<option>3</option>
+														<option>4</option>
+														<option>5</option>
+													</select>
+												</div>
+												<div class="form-group mb-30">
+													<label class="control-label mb-10 text-left">Radio</label>
+													<div class="radio radio-info">
+														<input type="radio" name="radio" id="radio1" value="option1" checked="">
+														<label for="radio1">
+															M
+														</label>
+													</div>
+													<div class="radio radio-info">
+														<input type="radio" name="radio" id="radio2" value="option2" checked="">
+														<label for="radio2">
+															F
+														</label>
+													</div>	
+												</div>
+												<div class="form-group mb-30">
+													<label class="control-label mb-10 text-left">Checkbox</label>
+													<div class="checkbox">
+														<input id="checkbox1" type="checkbox">
+														<label for="checkbox1">
+															Default
+														</label>
+													</div>
+													<div class="checkbox checkbox-primary">
+														<input id="checkbox2" type="checkbox" checked="">
+														<label for="checkbox2">
+															Primary
+														</label>
+													</div>
+													<div class="checkbox checkbox-success">
+														<input id="checkbox3" type="checkbox">
+														<label for="checkbox3">
+															Success
+														</label>
+													</div>	
+												</div>
+												<div class="form-group mb-30">
+													<label class="control-label mb-10 text-left">File upload</label>
+													<div class="fileinput fileinput-new input-group" data-provides="fileinput">
+														<div class="form-control" data-trigger="fileinput"> <i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>
+														<span class="input-group-addon fileupload btn btn-info btn-anim btn-file"><i class="fa fa-upload"></i> <span class="fileinput-new btn-text">Select file</span> <span class="fileinput-exists btn-text">Change</span>
+														<input type="file" name="...">
+														</span> <a href="#" class="input-group-addon btn btn-danger btn-anim fileinput-exists" data-dismiss="fileinput"><i class="fa fa-trash"></i><span class="btn-text"> Remove</span></a> 
+													</div>
+												</div>
+												<div class="form-group mb-0">
+													<label class="control-label mb-10 text-left">Helping text</label>
+													<input type="text" class="form-control" placeholder="Helping text">
+													<span class="help-block mt-10 mb-0"><small>A block of help text that breaks onto a new line and may extend beyond one line.</small></span>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				
 				</div>
 				
@@ -972,7 +1012,7 @@
 				<footer class="footer container-fluid pl-30 pr-30">
 					<div class="row">
 						<div class="col-sm-12">
-							<p><?=date('Y');?>&copy; KumalaGroup</p>
+							<p>2017 &copy; Hound. Pampered by Hencework</p>
 						</div>
 					</div>
 				</footer>
